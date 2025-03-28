e6a47fdb0a452b141355601b94dae0ffcff2ea82
Duc <traungo456@gmail.com>
2025-03-28 23:08:35 +0700

    4:28:30

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {addFood } from '../controllers/foodController.js';
+import {addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
 
 const foodRouter = express.Router();
@@ -17,31 +17,8 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list",listFood);
+foodRouter.post("/remove", removeFood);
+
 
 export default foodRouter;
-
-// import express from 'express';
-// import {addFood } from '../controllers/foodController.js';
-// import multer from 'multer';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// // Get __dirname equivalent in ES modules
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const foodRouter = express.Router();
-
-// // Image Storage Engine
-// const storage = multer.diskStorage({
-//     destination: path.join(__dirname, '../uploads'),
-//     filename: (req, file, cb) => {
-//         return cb(null, `${Date.now()}${file.originalname}`);
-//     }
-// });
-
-// const upload = multer({storage: storage});
-
-// foodRouter.post("/add", upload.single("image"), addFood);
-
-// export default foodRouter;
