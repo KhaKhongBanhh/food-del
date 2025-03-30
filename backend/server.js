9633f5fd340f1739299c76e85673841478a6528a
Duc <traungo456@gmail.com>
2025-03-30 20:19:26 +0700

    8:34:00

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+import 'dotenv/config'; // npm install dotenv
+
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 import 'dotenv/config';
 
 const app = express();
@@ -21,6 +24,8 @@ app.use('/api/food', foodRouter);
 app.use('/images', express.static('uploads'));
 app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+
 
 app.get('/', (req, res) => {
   res.send('API working!');
